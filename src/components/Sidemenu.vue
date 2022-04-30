9cce8c8d6ade865b32f6b2c4bba797b8ce7c9c7d
DukeofCambridge <68137344+DukeofCambridge@users.noreply.github.com>
2022-04-30 20:14:47 +0800

    锚点双向定位

--- src/components/Sidemenu.vue
+++ src/components/Sidemenu.vue
@@ -1,24 +1,24 @@
 <template>
   <el-menu
-      default-active="1"
+      :default-active=active
       style="width: 110px;opacity: 0.8">
     <el-menu-item index="1" @click="goAnchor('#top')">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-user"></i>
       <span >顶 部</span>
     </el-menu-item>
 
     <el-menu-item index="2" @click="goAnchor('#footprint')">
-      <i class="el-icon-menu"></i>
+      <i class="el-icon-guide"></i>
       <span >足 迹</span>
     </el-menu-item>
 
     <el-menu-item index="3" @click="goAnchor('#collection')">
-      <i class="el-icon-document"></i>
+      <i class="el-icon-medal"></i>
       <span >撷 英</span>
     </el-menu-item>
 
     <el-menu-item index="4" @click="goAnchor('#writing')">
-      <i class="el-icon-setting"></i>
+      <i class="el-icon-notebook-1"></i>
       <span >墨 痕</span>
     </el-menu-item>
   </el-menu>
@@ -27,11 +27,33 @@
 <script>
 export default {
   name: "Sidemenu",
+  data(){
+    return {
+      active: '1'
+    }
+  },
+  mounted() {
+    document.addEventListener('scroll', this.listenScroll)
+  },
   methods:{
     goAnchor (selector) {
       document.querySelector(selector).scrollIntoView({
         behavior:"smooth"
       })
+    },
+    // 内容部分滚动事件
+    listenScroll() {
+      // console.log('footprint:', 0>document.querySelector('#footprint').getBoundingClientRect().top)
+      // console.log(this.active)
+      switch (true) {
+        case 0>document.querySelector('#writing').getBoundingClientRect().top:
+          this.active='4';break;
+        case 0>document.querySelector('#collection').getBoundingClientRect().top:
+          this.active='3';break;
+        case 0>document.querySelector('#footprint').getBoundingClientRect().top:
+          this.active='2';break;
+        default: this.active='1';
+      }
     }
   }
 }
