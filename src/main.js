98b6e1516c2d8c78331fb664948aa2d211c8224b
DukeofCambridge <68137344+DukeofCambridge@users.noreply.github.com>
2022-08-04 22:25:28 +0800

    css修改

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
 import zhCn from 'element-plus/es/locale/lang/zh-cn'
-import 'element-ui/lib/theme-chalk/index.css'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 
